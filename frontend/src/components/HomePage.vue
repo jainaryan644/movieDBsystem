6f0b0ab6ab3f46dddb7c0e75c3a6d123a0337f68
jainaryan644 <62453609+jainaryan644@users.noreply.github.com>
2024-12-04 00:12:28 -0700

    Update and rename HelloWorld.vue to HomePage.vue

---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home-page">
+    <NavBar />
+    <SearchBar />
+    <PopularMovies />
+  </div>
+</template>
+
+<script>
+import NavBar from "./NavBar.vue";
+import SearchBar from "./SearchBar.vue";
+import PopularMovies from "./PopularMovies.vue";
+
+export default {
+  components: {
+    NavBar,
+    SearchBar,
+    PopularMovies,
+  },
+};
+</script>
