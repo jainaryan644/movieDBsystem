0b44d25d1fe84083979c8ebea67a99097ac97be9
Ryan Adams <wadams7@asu.edu>
2024-12-04 16:00:53 -0700

    Updated movie details to show the average rating in numerical form (may be a long floating point number, will look at formatting later)

--- frontend/src/components/MovieDetails.vue
+++ frontend/src/components/MovieDetails.vue
@@ -4,6 +4,7 @@
         <img :src="movie.poster" alt="Movie Poster" v-if="movie.poster" />
         <span class="simpleBox">
             <span v-for="n in 5" :key="n" :class="{'yellowStar':n <= avg_rating_rounded}">★</span>
+            <span> {{ movie.avg_rating }}</span>
         </span>
         <p>{{ movie.description }}</p>
     </div>
