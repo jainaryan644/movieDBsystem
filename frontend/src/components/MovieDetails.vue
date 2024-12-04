81d16659d58613564a3b367fd28749c42eaf3f41
Ryan Adams <wadams7@asu.edu>
2024-12-04 15:57:42 -0700

    Display an average rating for the movie based on reviews in star form

--- frontend/src/components/MovieDetails.vue
+++ frontend/src/components/MovieDetails.vue
@@ -1,14 +1,28 @@
 <template>
     <div class="movie-details">
-      <h1>{{ movie.title }}</h1>
-      <img :src="movie.poster" alt="Movie Poster" v-if="movie.poster" />
-      <p>{{ movie.description }}</p>
+        <h1>{{ movie.title }}</h1>
+        <img :src="movie.poster" alt="Movie Poster" v-if="movie.poster" />
+        <span class="simpleBox">
+            <span v-for="n in 5" :key="n" :class="{'yellowStar':n <= avg_rating_rounded}">★</span>
+        </span>
+        <p>{{ movie.description }}</p>
     </div>
-  </template>
+</template>
   
-  <script>
-  export default {
-    props: ["movie"],
-  };
-  </script>
-  
+<script>
+    export default {
+        props: ["movie"],
+        return {
+            avg_rating_rounded = Math.round(movie.avg_rating)
+        };
+    };
+</script>
+
+<style scoped>
+    .simpleBox{
+        outline: 3px solid black;
+    }
+    .yellowStar{
+        color: gold;
+    }
+</style>
