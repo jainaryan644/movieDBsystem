b33a5533bd4e0885d3788125eeb25e106541af63
jainaryan644 <62453609+jainaryan644@users.noreply.github.com>
2024-12-04 00:12:47 -0700

    Update and rename MovieDetails.vue to LoginPage.vue

---
+++ frontend/src/components/LoginPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login-page">
+    <h1>fiMDB</h1>
+    <form @submit.prevent="handleLogin">
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" id="username" v-model="username" placeholder="Enter your username" />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="password" placeholder="Enter your password" />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    handleLogin() {
+      // Authentication logic here
+      console.log("Logging in with:", this.username, this.password);
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add styles for your login form here */
+</style>
