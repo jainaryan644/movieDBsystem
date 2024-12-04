081af478d482dc6b82af562be7da5a4a4b646abd
jainaryan644 <62453609+jainaryan644@users.noreply.github.com>
2024-12-04 00:13:31 -0700

    Update and rename TestMovies.vue to PopularMovies.vue

---
+++ frontend/src/components/PopularMovies.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="popular-movies">
+    <h2>Popular Movies</h2>
+    <ul>
+      <li v-for="movie in topMovies" :key="movie.id">{{ movie.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      topMovies: [], // Populate this with API data
+    };
+  },
+  mounted() {
+    // Fetch top 5 movies here
+  },
+};
+</script>
