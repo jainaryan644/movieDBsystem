9e193b73dbacf242dc9b31967528029a8e81ada6
Aryan Jain <jainaryan644@gmail.com>
2024-12-06 04:37:55 -0700

    search bar functionality fixed, added further styling everywhere

--- frontend/src/components/MoviePage.vue
+++ frontend/src/components/MoviePage.vue
@@ -1,56 +1,53 @@
 <template>
-    <NavBar />
-    <MovieDetails v-if="movie" :movie="movie"/>
-    <!-- <div v-if="movie"> 
-      <h1>{{ movie.title }}</h1>
-      <p>Released: {{ movie.release_date }}</p>
-      <p>Rating: {{ movie.avg_rating }}</p>
-      <ReviewSection :movieId="movie.mid" />
-    </div> -->
-    <div v-else>
-      <p>Loading movie details...</p>
-    </div>
-    <ReviewSection v-if="movie" :movieId="movie.mid"/>
-  </template>
-  
-  <script>
-  import ReviewSection from "./ReviewSection.vue";
-  import MovieDetails from "./MovieDetails.vue";
-  import NavBar from "./NavBar.vue";
-  import axios from "axios";
-  
-  export default {
-    components: {
-      ReviewSection,
-      MovieDetails,
-      NavBar,
+  <NavBar />
+  <MovieDetails v-if="movie" :movie="movie"/>
+  <div v-else>
+    <p>Loading movie details...</p>
+  </div>
+  <ReviewSection v-if="movie" :movieId="movie.mid"/>
+</template>
+
+<script>
+import ReviewSection from "./ReviewSection.vue";
+import MovieDetails from "./MovieDetails.vue";
+import NavBar from "./NavBar.vue";
+import axios from "axios";
+
+export default {
+  components: {
+    ReviewSection,
+    MovieDetails,
+    NavBar,
+  },
+  data() {
+    return {
+      movie: null, // Movie details
+    };
+  },
+  async mounted() {
+    await this.fetchMovieDetails(this.$route.params.mid);
+  },
+  watch: {
+    $route(to, from) {
+      // Only fetch movie details if the `mid` has changed
+      if (to.params.mid !== from.params.mid) {
+        this.fetchMovieDetails(to.params.mid);
+      }
     },
-    data() {
-      return {
-        movie: null, // Movie details
-      };
+  },
+  methods: {
+    async fetchMovieDetails(movieId) {
+      try {
+        const response = await axios.get(`http://127.0.0.1:5000/movies/${movieId}`);
+        this.movie = response.data;
+      } catch (error) {
+        console.error("Error fetching movie details:", error);
+      }
     },
-    async mounted() {
-      //const movieId = this.$route.params.mid; // Get movie ID from route
-      await this.fetchMovieDetails(this.$route.params.mid);
-      // if (movieId) {
-      //   this.fetchMovieDetails(movieId);
-      // }
-    },
-    methods: {
-      async fetchMovieDetails(movieId) {
-        try {
-          const response = await axios.get(`http://127.0.0.1:5000/movies/${movieId}`);
-          this.movie = response.data;
-        } catch (error) {
-          console.error("Error fetching movie details:", error);
-        }
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  /* Add any specific styles here */
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped>
+/* Add any specific styles here */
+</style>
--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="search-bar">
-    <input type="text" v-model="query" placeholder="Search for movie..." @input="searchMovies" />
-    <ul v-if="results.length">
-      <li v-for="movie in results" :key="movie.title"><router-link v-bind:to="'/movie/' + movie.mid">{{ movie.title }}</router-link></li>
+    <input
+      type="text"
+      v-model="query"
+      placeholder="Search for movie..."
+      @input="searchMovies"
+      class="search-input"
+    />
+    <ul v-if="results.length" class="search-results">
+      <li v-for="movie in results" :key="movie.mid">
+        <router-link
+          :to="'/movie/' + movie.mid"
+          @click="clearSearch"
+          class="search-result-item"
+        >
+          {{ movie.title }}
+        </router-link>
+      </li>
     </ul>
   </div>
 </template>
@@ -17,21 +31,74 @@ export default {
   },
   methods: {
     async searchMovies() {
-      const response = await fetch("http://127.0.0.1:5000/movies/search/" + this.query);
-      const data = await response.json();
-      
-      this.results = data.results;
+      if (this.query.trim() === "") {
+        this.results = [];
+        return;
+      }
+      try {
+        const response = await fetch(
+          "http://127.0.0.1:5000/movies/search/" + encodeURIComponent(this.query)
+        );
+        if (response.ok) {
+          const data = await response.json();
+          this.results = data.results || [];
+        } else {
+          console.error("Error fetching search results");
+          this.results = [];
+        }
+      } catch (error) {
+        console.error("Search error:", error);
+        this.results = [];
+      }
+    },
+    clearSearch() {
+      this.query = "";
+      this.results = [];
     },
   },
 };
 </script>
 
 <style scoped>
-ul {
+.search-bar {
+  position: relative;
+  width: 600px;
+}
+
+.search-input {
+  width: 100%;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  outline: none;
+  font-size: 1rem;
+}
+
+.search-results {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
   background-color: white;
-  margin-top: 0%;
-  list-style-type: none; 
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  max-height: 200px;
+  overflow-y: auto;
+  z-index: 1000;
+  list-style: none;
   padding: 0;
-  
+  margin: 0;
+}
+
+.search-result-item {
+  display: block;
+  padding: 0.5rem;
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+}
+
+.search-result-item:hover {
+  background-color: #f0f0f0;
 }
 </style>
